cb2716bbfe82c197e1184e0038fe8f3b1bcc662a
zzss <zsolt.kocsi@gmail.com>
2015-12-24 03:51:03 +0100

    Add example

---
+++ paperwork-example/src/main/java/hu/supercluster/paperwork/example/MainActivity.java
@@ -0,0 +1,32 @@
+package hu.supercluster.paperwork.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import hu.supercluster.paperwork.Paperwork;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.gitSha) TextView gitSha;
+    @Bind(R.id.buildTime) TextView buildTime;
+    @Bind(R.id.extra) TextView extra;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        showPaperwork();
+    }
+
+    private void showPaperwork() {
+        final Paperwork paperwork = new Paperwork(this);
+
+        gitSha.setText(paperwork.getGitSha());
+        buildTime.setText(paperwork.getBuildTime());
+        extra.setText(paperwork.getExtra().toString());
+    }
+}
