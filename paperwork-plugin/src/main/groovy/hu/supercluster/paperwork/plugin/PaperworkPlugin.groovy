9ca6c9d7ffef7f4495736307942b9aaacd8b5423
Zsolt Kocsi <zsolt.kocsi@gmail.com>
2016-01-06 21:49:55 +0100

    Remove defaults, add generic data handling

--- paperwork-plugin/src/main/groovy/hu/supercluster/paperwork/plugin/PaperworkPlugin.groovy
+++ paperwork-plugin/src/main/groovy/hu/supercluster/paperwork/plugin/PaperworkPlugin.groovy
@@ -1,5 +1,6 @@
 package hu.supercluster.paperwork.plugin
 
+import groovy.json.JsonOutput
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -10,12 +11,9 @@ class PaperworkPlugin implements Plugin<Project> {
         project.afterEvaluate({
             File file = getFile(project)
 
-            def gitSha = project.paperwork.gitSha
-            def buildTime = project.paperwork.buildTime
-            def extras = project.paperwork.extras
-            def env = getEnv(project)
+            def set = project.paperwork.set
+            def paperwork = JsonOutput.toJson(set)
 
-            def paperwork = "{\"gitSha\":\"$gitSha\", \"buildTime\":\"$buildTime\", \"env\":{$env}, \"extras\":$extras}"
             file.write paperwork
         })
     }
@@ -24,20 +22,5 @@ class PaperworkPlugin implements Plugin<Project> {
         def file = project.file(project.paperwork.filename)
         file.parentFile.mkdirs()
         file
-    }
-
-    private String getEnv(Project project) {
-        def env = ''
-
-        project.paperwork.env.each { var ->
-            def value = System.getenv(var)
-            env = env + "\"${var}\":\"${value}\","
-        }
-
-        if (env.length() > 0) {
-            env = env.substring(0, env.length() - 1)
-        }
-
-        env
     }
 }
--- paperwork-runtime/src/main/java/hu/supercluster/paperwork/Paperwork.java
+++ paperwork-runtime/src/main/java/hu/supercluster/paperwork/Paperwork.java
@@ -13,10 +13,6 @@ import java.io.InputStreamReader;
 
 public class Paperwork {
     public static final String DEFAULT_FILENAME = "paperwork.json";
-    public static final String GIT_SHA = "gitSha";
-    public static final String BUILD_TIME = "buildTime";
-    public static final String ENV = "env";
-    public static final String EXTRAS = "extras";
 
     private final Context context;
     private String filename;
@@ -35,53 +31,21 @@ public class Paperwork {
         this.filename = filename;
     }
 
-    public String getGitSha() {
-        initAndCheck();
+    public String get(String key) {
+        init();
 
-        return json.optString(GIT_SHA);
+        return json.optString(key);
     }
 
-    public String getBuildTime() {
-        initAndCheck();
-
-        return json.optString(BUILD_TIME);
-    }
-
-    public String getEnv(String key) {
-        initAndCheck();
-
-        return json.optJSONObject(ENV).optString(key);
-    }
-
-    public JSONObject getExtras() {
-        initAndCheck();
-
-        return json.optJSONObject(EXTRAS);
-    }
-
-    public String getExtra(String key) {
-        initAndCheck();
-
-        return getExtras().optString(key);
-    }
-
-    private void initAndCheck() {
+    private void init() {
         if (json == null) {
             try {
-                json = getJsonContents();
+                json = new JSONObject(getFileContents());
 
             } catch (JSONException e) {
                 throw new PaperworkException("The file '%s' contains invalid JSON data", filename, e);
             }
-
-            check(json);
         }
-    }
-
-    private JSONObject getJsonContents() throws JSONException {
-        JSONObject jsonObject = new JSONObject(getFileContents());
-
-        return jsonObject;
     }
 
     @NonNull
@@ -105,15 +69,5 @@ public class Paperwork {
         }
 
         return builder.toString();
-    }
-
-    private void check(JSONObject jsonObject) {
-        if (!jsonObject.has(GIT_SHA) ||
-            !jsonObject.has(BUILD_TIME) ||
-            !jsonObject.has(ENV) ||
-            !jsonObject.has(EXTRAS)) {
-
-            throw new PaperworkException("The file '%s' is missing paperwork data", filename);
-        }
     }
 }
