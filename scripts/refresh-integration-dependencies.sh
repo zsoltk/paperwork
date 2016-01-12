e3cbffd94d140250598fcacbb71ba3b1dae46297
Zsolt Kocsi <zsolt.kocsi@gmail.com>
2016-01-12 20:24:12 +0100

    Extract refresh-integration-dependencies.sh

---
+++ scripts/refresh-integration-dependencies.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp paperwork-plugin/src/main/groovy/* buildSrc/src/main/groovy/ -r
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-cp paperwork-plugin/src/main/groovy/* buildSrc/src/main/groovy/ -r
-
 ./gradlew clean \
           build \
           :paperwork-plugin:test \
